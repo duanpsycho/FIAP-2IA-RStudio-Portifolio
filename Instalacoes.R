144edad128f34131359981ba52bc89832fae39e8
Duan Nunes Alves da Silva <39650422+duanpsycho@users.noreply.github.com>
2018-08-20 22:25:56 -0300

    Add files with upload
    First upload.

---
+++ Instalacoes.R
@@ -0,0 +1,18 @@
+install.packages("dplyr", dependencies = TRUE)
+install.packages("readr", dependencies = TRUE)
+install.packages("plotly", dependencies = TRUE)
+install.packages("zoo", dependencies = TRUE)
+install.packages("scales", dependencies = TRUE)
+install.packages("dendextend", dependencies = TRUE)
+install.packages("ape", dependencies = TRUE)
+install.packages("scales", dependencies = TRUE)
+install.packages("tidyverse", dependencies = TRUE)
+install.packages("tidyr", dependencies = TRUE)
+install.packages("caret", dependencies = TRUE)
+install.packages("kernlab", dependencies = TRUE)
+install.packages("xlsx", dependencies = TRUE)
+install.packages("openxlsx", dependencies = TRUE)
+install.packages("e1071", dependencies = TRUE)
+
+# macOS
+# Executar sudo ln -f -s $(/usr/libexec/java_home)/jre/lib/server/libjvm.dylib /usr/local/lib
